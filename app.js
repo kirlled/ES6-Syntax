c155a5dcf1faa195c3d2055f8552d5edd1ce9a4b
Kirlled Anderson Araque <dersonsoft@gmail.com>
2018-12-17 22:56:22 -0400

    Improving with good practices

--- app.js
+++ app.js
@@ -8,22 +8,23 @@ class Operations{
    * Does a split, then a reverse and finally a join.
    * It has a cost of O(n), however, it's more efficient than to use
    * the three native methods with concatenation
-   *
+   * 
    * @param  {String} string  String to be operated
    * @param  {String} search  Character to split
    * @param  {String} replace String to replace the character's wanted 
    * @return {String}         String operated
    */
-  static splitReverseJoin(string, search = '-', replace = '/'){
-    var [tmp, result] = ['', ''];
+  static splitReverseJoin(string, search = "-", replace = "/"){
+    var [tmp, result] = ["", ""];
+    var strLength     = string.length;
 
-    for (let i = 0;  i < string.length; i++){
+    for (let i = 0;  i < strLength; i++){
       if (string[i] != search)
         tmp += string[i];
       
       else{
         result = replace + tmp + result;
-        tmp  = '';
+        tmp  = "";
       }
     }
 
@@ -72,15 +73,14 @@ class Person{
    * @return String [format dd/mm/yyyy]
    */
   getBirthdate(){
-    return this.birthdate.split('-').reverse().join('/');
+    return this.birthdate.split("-").reverse().join("/");
   }
 }
 
 class Developer extends Person{
-  constructor (nombre, sex, birthdate, country, position, stack){
-    super(nombre, sex, birthdate, country);
-    
-    this.nombre    = nombre;
+  constructor (name, sex, birthdate, country, position, stack){
+    super(name, sex, birthdate, country);
+    this.name      = name;
     this.sex       = sex;
     this.birthdate = birthdate;
     this.country   = country;
@@ -89,144 +89,125 @@ class Developer extends Person{
   }
 }
 
-var learningTech = [];
-
-function learning(...technologies){
-  technologies.forEach((tech) => learningTech.push(tech));
+function arrFill(arr, ...data){
+  data.forEach((tech) => arr.push(tech));
 }
 
-var dev = new Developer('Kirlled', 'M', '1995-40-40', 'Venezuela', 'Full Stack' ,'LEMP');
-dev.website = 'kirlled.com';
-dev.email   = 'kirlledaraque@gmail.com';
+function printSubTitle(title){
+  console.log(`
+  ${title}:
+  `);
+}
+
+function printSkills(title, skills){
+  console.log(`
+  ${title}
+  ----------------------`);
+  skills.forEach((skill) => console.log("* "+ skill));
+}
+
+var arrLearning = [];
+var dev = new Developer("Kirlled", "M", "1995-40-40", "Venezuela", 
+                                        "Full Stack" ,"LEMP");
+dev.website = "kirlled.com";
+dev.email   = "kirlledaraque@gmail.com";
 
 var backend = new Set();
-backend.add('PHP');
-backend.add('Laravel');
-backend.add('C');
-backend.add('C++');
-backend.add('Java');
+backend.add("PHP");
+backend.add("Laravel");
+backend.add("C");
+backend.add("C++");
+backend.add("Java");
 
 var frontend = new Set();
-frontend.add('HTML5');
-frontend.add('CSS3');
-frontend.add('FlexBox');
-frontend.add('SaSS');
-frontend.add('Bootstrap');
-frontend.add('JavaScript');
-frontend.add('jQuery');
-frontend.add('AJAX');
-frontend.add('VueJs');
+frontend.add("HTML5");
+frontend.add("CSS3");
+frontend.add("FlexBox");
+frontend.add("SaSS");
+frontend.add("Bootstrap");
+frontend.add("JavaScript");
+frontend.add("jQuery");
+frontend.add("AJAX");
+frontend.add("VueJs");
 
 var databases = new Set();
-databases.add('MySQL');
+databases.add("MySQL");
 
 var sysadmin = new Set();
-sysadmin.add('Linux');
-sysadmin.add('Ubuntu Server');
-sysadmin.add('Nginx');
-sysadmin.add('Apache');
-sysadmin.add('Tomcat');
-sysadmin.add('Security');
-sysadmin.add('Firewall');
+sysadmin.add("Linux");
+sysadmin.add("Ubuntu Server");
+sysadmin.add("Nginx");
+sysadmin.add("Apache");
+sysadmin.add("Tomcat");
+sysadmin.add("Security");
+sysadmin.add("Firewall");
 sysadmin.add("Let's Encrypt");
 sysadmin.add("Bash Scripting");
 
 var others = new Set();
-others.add('Git');
-others.add('Scrum');
-others.add('SEO');
-others.add('GulpJS');
-others.add('cURL');
-others.add('POO');
-others.add('Composer');
-others.add('npm');
+others.add("Git");
+others.add("Scrum");
+others.add("SEO");
+others.add("GulpJS");
+others.add("cURL");
+others.add("POO");
+others.add("Composer");
+others.add("npm");
 
 var aboutMe = `Hi! my name is ${dev.name} I'm from ${dev.country}, 
 I'm ${dev.getAge()} years old, I'm a ${dev.position} Developer, 
 I usually use the ${dev.stack} stack to work.`;
-
 console.log(aboutMe);
-console.log(`
-My Skills:
-`);
 
-console.log(`
-Backend
-----------------------`);
-backend.forEach((skill) => console.log('* '+ skill));
-
-console.log(`
-Frontend
-----------------------`);
-frontend.forEach((skill) => console.log('* '+ skill));
-
-console.log(`
-SysAdmin
-----------------------`);
-sysadmin.forEach((skill) => console.log('* '+ skill));
-
-console.log(`
-Others
-----------------------`);
-others.forEach((skill) => console.log('* '+ skill));
+printSubTitle('My Skills');
+printSkills("Backend", backend);
+printSkills("Frontend", frontend);
+printSkills("SysAdmin", sysadmin);
+printSkills("Others", others);
 
 console.log(`
 Learning
 ----------------------`);
 
-learning('NodeJS', 'React', 'WebSockets', 'Express', 'MongoDB');
+arrFill(arrLearning, "NodeJS", "React", "WebSockets", "Express", "MongoDB");
 
-for (tech of learningTech){
+for (tech of arrLearning){
   console.log(tech);
 }
 
+printSubTitle('Running some Tests...');
 
-console.log(`
-Running some Tests...
-`);
-
-if (Operations.splitReverseJoin('06-07-10-70') == '70/10/07/06')
-  console.log('OK');
+if (Operations.splitReverseJoin("06-07-10-70") == "70/10/07/06")
+  console.log("OK");
 else
-  console.log('ERROR');
+  console.log("ERROR");
 
-if (Operations.splitReverseJoin('06.07.10.70', '.', ',') == '70,10,07,06')
-  console.log('OK');
+if (Operations.splitReverseJoin("06.07.10.70", ".", ",") == "70,10,07,06")
+  console.log("OK");
 else
-  console.log('ERROR');
+  console.log("ERROR");
 
 if (Operations.splitReverseJoin(dev.birthdate) == dev.getBirthdate())
-  console.log('OK');
+  console.log("OK");
 else
-  console.log('ERROR');
-
-if (Number.isInteger(dev.getYearOfBirth()))
-  console.log('OK');
-else
-  console.log('ERROR');
+  console.log("ERROR");
 
 if (dev.getYearOfBirth().toString().length == 4)
-  console.log('OK');
+  console.log("OK");
 else
-  console.log('ERROR');
+  console.log("ERROR");
 
-console.log(`
-If you want to contact me you can do it through my website ${dev.website} or my email ${dev.email}
-Regards!
-`);
-
+Number.isInteger(dev.getYearOfBirth()) ? console.log("OK") : 
+                                         console.log("ERROR");
 
 var promesa = new Promise(function(resolve, reject){
-  var fruits = ['Apple', 'Pear', 'Grape', 'Strawberry', 'Lemon'];
+  var fruits = ["Apple", "Pear", "Grape", "Strawberry", "Lemon"];
   c = 0;
 
   for (fruit of fruits)
     c++;
 
-  if (c > 4)
-    resolve('Success');
-  else
-    reject(Error('Something has gone wrong'));
+  c > 4 ? resolve("Success") : reject(Error("Something has gone wrong"));
 });
 
 promesa.then(function(result){
@@ -234,3 +215,10 @@ promesa.then(function(result){
 }, function(error){
   console.log(error);
 });
+
+console.log(`
+If you want to contact me you can do it 
+through my website ${dev.website} 
+or my email ${dev.email}
+Regards!
+`);
